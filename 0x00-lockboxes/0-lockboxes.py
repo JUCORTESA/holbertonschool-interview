9411c575263834ab061fdbdbee50ec7b304e70f0
jucortesa <984@holbertonschool.com>
2020-04-19 20:45:07 -0500

    [feature] lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'Lockboxes'
+
+
+def canUnlockAll(boxes):
+    """method that determines if all the boxes can be opened
+
+    Arguments:
+        boxes is a list of lists
+        A key with the same number as a box opens that box
+
+    Returns:
+        True if all boxes can be opened, else return False
+    """
+    checkbox = {}
+    stack = [0]
+    aux = []
+
+    for key in range(len(boxes)):
+        checkbox[key] = boxes[key]
+        aux.append(key)
+    for box, arr in checkbox.items():
+        box = 0
+        for num in arr:
+            if box in stack:
+                box = num
+                if num not in stack:
+                    stack.append(num)
+                    break
+    stack.sort()
+    if stack == aux:
+        return True
+    else:
+        return False

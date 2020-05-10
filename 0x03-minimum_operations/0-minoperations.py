7340eb3b96f25d42e633655bb436a59592fedde9
jucortesa <984@holbertonschool.com>
2020-05-09 20:11:22 -0500

    [feature] minimun operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Minimum Operations'''
+
+
+def minOperations(n):
+    '''Method that calculates the fewest number of operations
+    needed to result in exactly n H characters in the file.
+    Arguments:
+        n - character
+    Returns:
+        an integer
+    '''
+    if type(n) is not int or n < 2:
+        return 0
+    operation = 0
+    for i in range(2, n):
+        while(n % i == 0):  # check if problem can be broken into smaller prob
+            operation += i  # if yes then add n of smaller problems to result.
+            n = n/i
+    return operation

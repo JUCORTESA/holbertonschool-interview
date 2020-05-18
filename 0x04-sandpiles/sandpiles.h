871e5ad5e61378d12d04e8d6935299ae56857ab3
jucortesa <984@holbertonschool.com>
2020-05-18 11:05:11 -0500

    header

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,9 +1,9 @@
-#ifndef SANDPILES_H
-#define SANDPILES_H
+#ifndef _SANDPILES_H
+#define _SANDPILES_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 
-#endif /* SANDPILES_H*/
+#endif /* _SANDPILES_H */

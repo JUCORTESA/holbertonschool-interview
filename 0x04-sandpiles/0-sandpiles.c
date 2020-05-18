d5e1e7a41019e1b81c24adfc6b80390527da14d1
jucortesa <984@holbertonschool.com>
2020-05-18 11:55:34 -0500

    [update] change int in loop

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -8,9 +8,11 @@
  */
 void add(int grid1[3][3], int grid2[3][3])
 {
-	for (int x = 0; x < 3; x++)
+	int x, y;
+
+	for (x = 0; x < 3; x++)
 	{
-		for (int y = 0; y < 3; y++)
+		for (y = 0; y < 3; y++)
 		{
 			grid1[x][y] = grid1[x][y] + grid2[x][y];
 		}

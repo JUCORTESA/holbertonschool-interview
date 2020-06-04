7e31d204cd0fa71283168f4e8652accdcba5a0fa
jucortesa <984@holbertonschool.com>
2020-06-03 20:46:09 -0500

    [fix] condition printed

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,16 +3,15 @@
 import sys
 
 
-input = {'200': 0,
-         '301': 0,
-         '400': 0,
-         '401': 0,
-         '403': 0,
-         '404': 0,
-         '405': 0,
-         '500': 0}
-status_code = ""
-file_size = ""
+code = {'200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0}
+
 counter = 1
 sum_size = 0
 try:
@@ -22,17 +21,17 @@ try:
             status_code = aux[-2]
             file_size = int(aux[-1])
             sum_size += file_size
-            if status_code in input:
-                input[status_code] += 1
-            if counter % 10 == 0:
-                print("File size: {}".format(sum_size))
-                for key, value in input.items():
-                    if input[key] != 0:
-                        print("{}: {}".format(key, value))
+            if status_code in code:
+                code[status_code] += 1
+        if counter % 10 == 0:
+            print("File size: {}".format(sum_size))
+            for key, value in code.items():
+                if code[key] != 0:
+                    print("{}: {}".format(key, value))
         counter += 1
 except KeyboardInterrupt:
     print("File size: {}".format(sum_size))
-    for key, value in input.items():
-        if input[key] != 0:
+    for key, value in code.items():
+        if code[key] != 0:
             print("{}: {}".format(key, value))
     raise

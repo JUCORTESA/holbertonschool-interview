1f16f32f41b3cd391db715107f9e270698a816ed
jucortesa <984@holbertonschool.com>
2020-06-03 20:49:37 -0500

    [fix] change to try, escept, finally

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,6 +2,10 @@
 """Log Parse"""
 import sys
 
+import sys
+
+counter = 1
+sum_size = 0
 
 code = {'200': 0,
         '301': 0,
@@ -12,26 +16,30 @@ code = {'200': 0,
         '405': 0,
         '500': 0}
 
-counter = 1
-sum_size = 0
 try:
     for line in sys.stdin:
-        aux = line.split()
-        if len(aux) > 2:
-            status_code = aux[-2]
-            file_size = int(aux[-1])
-            sum_size += file_size
-            if status_code in code:
-                code[status_code] += 1
+        ln = line.split()
+
+        if len(ln) > 2:
+            status = ln[-2]
+            size = int(ln[-1])
+            sum_size += size
+            if status in code:
+                code[status] += 1
+
         if counter % 10 == 0:
             print("File size: {}".format(sum_size))
-            for key, value in code.items():
+            for key in sorted(code.keys()):
                 if code[key] != 0:
-                    print("{}: {}".format(key, value))
+                    print("{}: {}".format(key, code[key]))
+
         counter += 1
-except KeyboardInterrupt:
+
+except Exception:
+    pass
+
+finally:
     print("File size: {}".format(sum_size))
-    for key, value in code.items():
+    for key in sorted(code.keys()):
         if code[key] != 0:
-            print("{}: {}".format(key, value))
-    raise
+            print("{}: {}".format(key, code[key]))
